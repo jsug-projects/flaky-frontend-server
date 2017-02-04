306f0b3dfa90417bb566dbf0b1271dbbfa51aa96
making <makingx@gmail.com>
2017-02-04 23:05:29 +0900

    Polish

--- src/main/java/jsug/flaky/auth/UserInfoController.java
+++ src/main/java/jsug/flaky/auth/UserInfoController.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
-`import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.bind.annotation.GetMapping;
