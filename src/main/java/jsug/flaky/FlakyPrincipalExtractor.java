e3466f0e93072d76913fed13d3fddcd4270dca67
making <makingx@gmail.com>
2017-02-04 17:24:43 +0900

    Enable Zuul Proxy, rename module name

--- src/main/java/jsug/flaky/FlakyPrincipalExtractor.java
+++ src/main/java/jsug/flaky/FlakyPrincipalExtractor.java
@@ -5,27 +5,31 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import lombok.AllArgsConstructor;
-
 @Component
-@AllArgsConstructor
 public class FlakyPrincipalExtractor implements PrincipalExtractor {
 	private final RestTemplate restTemplate;
+	private final String membersApi;
+
+	public FlakyPrincipalExtractor(RestTemplate restTemplate,
+			@Value("${zuul.routes.members.url}") String membersApi) {
+		this.restTemplate = restTemplate;
+		this.membersApi = membersApi;
+	}
 
 	@Override
 	public Object extractPrincipal(Map<String, Object> map) {
 		String github = getValue(map, "login");
 		FlakyUser user;
 		try {
-			user = restTemplate.getForObject(
-					"http://localhost:8082/members/search/findByGithub?github={github}",
-					FlakyUser.class, github);
+			user = restTemplate.getForObject("{path}/search/findByGithub?github={github}",
+					FlakyUser.class, membersApi, github);
 		}
 		catch (HttpClientErrorException e) {
 			if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
--- src/main/java/jsug/flaky/FlakyUiApplication.java
+++ src/main/java/jsug/flaky/FlakyUiApplication.java
@@ -3,10 +3,12 @@ package jsug.flaky;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class FlakyUiApplication {
 
 	@Bean
