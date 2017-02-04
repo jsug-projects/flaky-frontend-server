9dc086b994ee6c50572eb9cbd33c57b1ea46d5a0
making <makingx@gmail.com>
2017-02-04 20:23:03 +0900

    Add me endpoint

--- src/main/java/jsug/flaky/auth/UserInfoController.java
+++ src/main/java/jsug/flaky/auth/UserInfoController.java
@@ -1,6 +1,8 @@
 package jsug.flaky.auth;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +16,15 @@ public class UserInfoController {
 
 	@GetMapping("userinfo")
 	ResponseEntity<?> userinfo(@RequestHeader("Authorization") String authorization) {
-		System.out.println(authorization);
 		String token = authorization.split(" ")[1];
 		return accessTokenMapping.getUser(token).map(ResponseEntity::ok)
 				.orElseGet(() -> ResponseEntity.notFound().build());
+	}
+
+	@GetMapping("me")
+	String userinfo(OAuth2Authentication authentication) {
+		OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) authentication
+				.getDetails();
+		return details.getTokenValue();
 	}
 }
