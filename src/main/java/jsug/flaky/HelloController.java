be94c1680bf055466962e82d63a2c78b2033a9dd
making <makingx@gmail.com>
2017-02-04 23:09:22 +0900

    Remove /

--- src/main/java/jsug/flaky/HelloController.java
+++ src/main/java/jsug/flaky/HelloController.java
@@ -2,17 +2,10 @@ package jsug.flaky;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-
-	@GetMapping("/")
-	String hello(@AuthenticationPrincipal FlakyUser flakyUser, Model model) {
-		model.addAttribute("user", flakyUser);
-		return "index";
-	}
 
 	@GetMapping("/event")
 	String event(@AuthenticationPrincipal FlakyUser flakyUser) {
